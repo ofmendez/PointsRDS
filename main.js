633a1897bf8b62373365a8bc0e3c8ffb4824b527
JFonsecaReddesign <desarrollo@reddesignsystems.com>
2023-09-16 04:27:57 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+//const {google} = require('googleapis');
+import {google} from 'googleapis';
+
+const serviceAccountKeyFile = "./steadfast-karma-177403-5484f69a7b47.json";
+const sheetId = '1-ZMr0QiqK4Nqmq5-rq3KUPJt7vwIpPK4elSld5KmDNo'
+const tabName = 'Septiembre'
+const range = 'A:E'
+
+main().then(() => {
+  console.log('Completed')
+})
+
+async function main() {
+  // Generating google sheet client
+  const googleSheetClient = await _getGoogleSheetClient();
+
+  // Reading Google Sheet from a specific range
+  const data = await _readGoogleSheet(googleSheetClient, sheetId, tabName, range);
+  console.log(data);
+
+  // Adding a new row to Google Sheet
+  const dataToBeInserted = [
+     ['11', 'rohith', 'Rohith', 'Sharma', 'Active'],
+     ['12', 'virat', 'Virat', 'Kohli', 'Active']
+  ]
+  await _writeGoogleSheet(googleSheetClient, sheetId, tabName, range, dataToBeInserted);
+}
+
+async function _getGoogleSheetClient() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: serviceAccountKeyFile,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  const authClient = await auth.getClient();
+  return google.sheets({
+    version: 'v4',
+    auth: authClient,
+  });
+}
+
+async function _readGoogleSheet(googleSheetClient, sheetId, tabName, range) {
+  const res = await googleSheetClient.spreadsheets.values.get({
+    spreadsheetId: sheetId,
+    range: `${tabName}!${range}`,
+  });
+
+  return res.data.values;
+}
+
+async function _writeGoogleSheet(googleSheetClient, sheetId, tabName, range, data) {
+  await googleSheetClient.spreadsheets.values.append({
+    spreadsheetId: sheetId,
+    range: `${tabName}!${range}`,
+    valueInputOption: 'USER_ENTERED',
+    insertDataOption: 'INSERT_ROWS',
+    resource: {
+      "majorDimension": "ROWS",
+      "values": data
+    },
+  })
+}
